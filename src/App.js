b235aa04a0c5a0c25639fd6df93c283fb2a4fdbd
Pablo Fernandez <pablo.ezequiel.fernandez@gmail.com>
2020-07-28 20:05:33 -0300

    Ocultando botones del navegador

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 
       const imagenesPorPagina = 30;
       const key = '17227505-5144ca8b6bc9b6f90b7a60061';
-      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
+      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}&page=${paginaactual}`;
   
       const respuesta = await fetch(url);
       const resultado = await respuesta.json();
@@ -27,10 +27,14 @@ function App() {
       // Calcular el total de paginas
       const calcularTotalPaginas = Math.ceil(resultado.totalHits / imagenesPorPagina );
       guardarTotalPaginas(calcularTotalPaginas);
+
+      // Mover la pantalla hacia arriba
+      const jumbotron = document.querySelector('.jumbotron');
+      jumbotron.scrollIntoView({ behavior: 'smooth' })
     }
     consultarApi();
 
-  }, [busqueda])
+  }, [busqueda, paginaactual])
 
   // Definir la pagina anterior
   const paginaAnterior = () => {
@@ -61,17 +65,22 @@ function App() {
           imagenes={imagenes}
         />
 
-        <button
-          type="button"
-          className="btn btn-info mr-1"
-          onClick={paginaAnterior}
-        >&laquo; Anterior </button>
+        { (paginaactual === 1) ? null : (
+          <button
+              type="button"
+              className="btn btn-info mr-1"
+              onClick={paginaAnterior}
+          >&laquo; Anterior </button>
+        )}
 
-        <button
-          type="button"
-          className="btn btn-info"
-          onClick={paginaSiguiente}
-        >Siguiente &raquo;</button>
+        { (paginaactual === totalpaginas) ? null : (
+          <button
+              type="button"
+              className="btn btn-info"
+              onClick={paginaSiguiente}
+          >Siguiente &raquo;</button>
+        )}
+
       </div>
     </div>
   );
